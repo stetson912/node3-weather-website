1f5de52e54babb1eae5dd9babedd27696856af56
Trenton Stetson <trenton_stetson@stu.centralaz.edu>
2021-11-21 21:18:02 -0700

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = "http://api.weatherstack.com/current?access_key=f4263c337a2d3f84c5fae5d04e88b1d9&query="
+        + latitude + ',' + longitude + "&units=f"
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback("Unable to connect to weather service.", undefined)
+        } else if (body.error) {
+            callback("Unable to find location.", undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+        }
+    })
+}
+
+module.exports = forecast
