3461da6f276336764401fb8b9ad439d60f85906a
Trenton Stetson <trenton_stetson@stu.centralaz.edu>
2021-11-21 22:03:57 -0700

    Setup app for heroku by changing port and domains

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,8 @@ weatherForm.addEventListener('submit', (e) => {
 
     messageOne.textContent = "Loading..."
     messageTwo.textContent = ""
-    fetch('http://localhost:3000/weather?address=' + location)
+
+    fetch('/weather?address=' + location)
         .then((response) => {
             response.json().then((data) => {
                 if (data.error) {
